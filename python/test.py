c3b31b7b9024141047651d1083bfaddd0da4b6f4
OSangKwon <OsangKwon@github.com>
2019-07-17 17:14:34 +0900

    servertest

--- python/test.py
+++ python/test.py
@@ -6,13 +6,13 @@ import socket
 ip_addr = '192.168.43.219'
 port = 8000
 
-url = "http://ec2-52-79-243-120.ap-northeast-2.compute.amazonaws.com:3000/python"
+#url = "http://ec2-52-79-243-120.ap-northeast-2.compute.amazonaws.com:3000/python"
 print('go')
 while True:
     print('test')
-    res = requests.get(url)
+    a = input()
+    #res = requests.get(url)
     
-    a=res.text
-    print(a)
+   # a=res.text
     a=a.encode('utf-8')
-    #sock.send(a)
+    sock.send(a)
